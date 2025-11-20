93966a9d3e944ff81d3c7d77aa6123f9d4b06c98
Shriyash <shriyashpatil24.work@gmail.com>
2025-11-20 11:40:30 +0530

    Triangle

--- Python Basics/Coding Exercises.py
+++ Python Basics/Coding Exercises.py
@@ -1,2 +1,6 @@
 # Area Of Triangle
 
+length = int(input("Enter the length of the triangle: "))
+height = int(input("Enter the height of the triangle: "))
+area = (length * height) / 2
+print("The area of the triangle is ", area)
