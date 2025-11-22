a6296373b5158d02854d924873cd45c07a7113f1
Shriyash-24 <shriyashpatil24.work@gmail.com>
2025-11-22 10:19:36 +0530

    Slicing

---
+++ 2-Strings-In-Python/Slicing.py
@@ -0,0 +1,17 @@
+s1 = "Hello World"
+"""
+print(s1)
+print(len(s1)) # Length
+print("First char", s1[0])
+print("Last char", s1[-1])
+"""
+
+# Syntax of Indexing - string[index]
+# Syntax Of String - string[start:end:step]
+# start - starting index at which slicing begins
+# end - Ending index at which slicing ends (excluded)
+# step - integer that specifies the increment/decrement of slicing
+print(s1[2:7:1])
+print(s1[2:9:2])
+print(s1[1:12:3])
+
