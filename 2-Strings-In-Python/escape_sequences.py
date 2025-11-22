5ea9877d7436e6c9b2594dd37123765a5f0fb210
Shriyash-24 <shriyashpatil24.work@gmail.com>
2025-11-22 10:44:19 +0530

    Escape Sequences

---
+++ 2-Strings-In-Python/escape_sequences.py
@@ -0,0 +1,14 @@
+# \n - new line
+# \t - tab
+# \\ - backslash
+# \' - single quote inside a single quoted string.
+# \" - double quote inside a double quoted string.
+
+print("Hello everyone.\nHow are you?")
+print("John\t20")
+print("New\\old") # We wanted to print New\old.
+
+print('This is Python\'s class')
+print("This is Python's class")
+
+print("He says, \"We are learning python.\"")
